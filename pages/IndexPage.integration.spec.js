9c10d6180556351fa5b7b26dbdd2cbac7978898b
Caio <caiojulian@hotmail.com>
2022-03-26 22:50:19 -0300

    test indexPage store

--- pages/IndexPage.integration.spec.js
+++ pages/IndexPage.integration.spec.js
@@ -1,15 +1,41 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex, { Store } from 'vuex'
 import IndexPage from '.'
 import MapCase from '@/components/MapCase.vue'
 import RangeDate from '@/components/RangeDate.vue'
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
 describe('IndexPage - unit', () => {
+  let store, caseManager
+
+  beforeEach(() => {
+    caseManager = {
+      namespaced: true,
+      state: {
+        mapData: {},
+      },
+      actions: {
+        fetchCases: jest.fn(),
+      },
+    }
+
+    store = new Store({
+      modules: {
+        caseManager,
+      },
+    })
+  })
+
   const mountIndexPage = () => {
     const wrapper = mount(IndexPage, {
       stubs: {
         MapCase,
         RangeDate,
       },
+      store,
+      localVue,
     })
 
     return wrapper
